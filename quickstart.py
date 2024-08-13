fb6a8744b828661eeb4ce698799c4bec949478d1
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-08-13 16:26:04 -0400

    delete file is exists

--- quickstart.py
+++ quickstart.py
@@ -1,7 +1,15 @@
 import os
 from pydrive.auth import GoogleAuth
 
-os.remove("credentials_module.json") # delete old acccess token file
+file_path = './credentials_module.json'
+
+# Verificar si el archivo existe
+if os.path.exists(file_path):
+    os.remove(file_path) # eliminar el archivo
+    print(f"El archivo {file_path} ha sido eliminado.")
+else:
+    print(f"El archivo {file_path} no existe.")
 
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+
